063340fa58cac0a38d217ff9cc46bf33d5f132df
lakhangupta <lakhangupta53223@gmail.com>
2025-08-16 17:31:15 +0530

    . .

--- config/db.js
+++ config/db.js
@@ -1,18 +1,18 @@
 // config/db.js
 const mongoose = require('mongoose');
 let mongoUrl = 'mongodb+srv://absdolfinlakhan1:TrIoH5zQw3NrCPro@cluster0.a3h2oai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0' //   ||  'mongodb://127.0.0.1:27017/myshop'
+// config/db.js
+
 const connectDB = async () => {
   try {
-    console.log(" by -> " ,mongoUrl );
-    await mongoose.connect(
-      mongoUrl, // Or your MongoDB Atlas URL
-      { useNewUrlParser: true, useUnifiedTopology: true }
-    );
-    console.log(' MongoDB connected');
+    console.log("Connecting to MongoDB at:", mongoUrl);
+    await mongoose.connect(mongoUrl); // No need for deprecated options
+    console.log('MongoDB connected successfully');
   } catch (err) {
-    console.error(' MongoDB connection error:', err.message);
+    console.error('MongoDB connection error:', err.message);
     process.exit(1);
   }
 };
 
 module.exports = connectDB;
+
