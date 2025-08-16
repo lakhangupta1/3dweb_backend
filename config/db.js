246645d2b5b113fb6ae2f146a27e3c1720e836f6
lakhangupta <lakhangupta53223@gmail.com>
2025-08-16 17:27:47 +0530

    add new mongo url

--- config/db.js
+++ config/db.js
@@ -1,15 +1,16 @@
 // config/db.js
 const mongoose = require('mongoose');
-
+let mongoUrl = 'mongodb+srv://absdolfinlakhan1:TrIoH5zQw3NrCPro@cluster0.a3h2oai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0' //   ||  'mongodb://127.0.0.1:27017/myshop'
 const connectDB = async () => {
   try {
+    console.log(" by -> " ,mongoUrl );
     await mongoose.connect(
-      'mongodb://127.0.0.1:27017/myshop', // Or your MongoDB Atlas URL
+      mongoUrl, // Or your MongoDB Atlas URL
       { useNewUrlParser: true, useUnifiedTopology: true }
     );
-    console.log('✅ MongoDB connected');
+    console.log(' MongoDB connected');
   } catch (err) {
-    console.error('❌ MongoDB connection error:', err.message);
+    console.error(' MongoDB connection error:', err.message);
     process.exit(1);
   }
 };
