7ed75f2c516ad379e624dd0020bf99a9113fc238
lakhangupta <lakhangupta53223@gmail.com>
2025-08-16 18:33:34 +0530

    ..

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 // config/db.js
 const mongoose = require('mongoose');
-let mongoUrl = 'mongodb+srv://absdolfinlakhan1:TrIoH5zQw3NrCPro@cluster0.a3h2oai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0' //   ||  'mongodb://127.0.0.1:27017/myshop'
+// let mongoUrl = 'mongodb+srv://absdolfinlakhan1:TrIoH5zQw3NrCPro@cluster0.a3h2oai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0' //   ||  'mongodb://127.0.0.1:27017/myshop'
 // config/db.js
+let mongoUrl  = 'mongodb+srv://absdolfinlakhan1:TrIoH5zQw3NrCPro@cluster0.a3h2oai.mongodb.net/myshop?retryWrites=true&w=majority&appName=Cluster0'
 
 const connectDB = async () => {
   try {
